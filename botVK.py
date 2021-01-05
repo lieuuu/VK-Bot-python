eca53d00779a405429ad73e46309fa628a105f52
n33ck33ch <76656108+xyanxi9ta@users.noreply.github.com>
2021-01-05 13:27:15 +0300

    Update botVK.py

--- botVK.py
+++ botVK.py
@@ -1,8 +1,8 @@
-import vk_api
-import json
-import time
+import vk_api #модуль работы с vk-api
+import json #модуль работы с json 
+import time #модуль работы со временем
 from vk_api.longpoll import  VkLongPoll,VkEventType
-key = "304edfe291a39e4be153976914532de6a24905e4b5e102805a7d31be4d3f045a4aca260e038b234d46873"
+key = "API - VK key in the group"
 vk_session = vk_api.VkApi(token = key)
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
@@ -27,23 +27,23 @@ def riddleFunction():# Чертеж прослушки
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             counter(event.user_id)
-            if event.text.lower().find("python" "" "пайтон") <= -1:
+            if event.text.lower().find("ответ на тест") <= -1: #можно использовать несколько ответов, но будут они писаться через ""
                 vk.messages.send(
                     user_id=event.user_id,
                     random_id=time.time(),
-                    message="Молодец , у тебя все получилось!")
+                    message="Молодец , у тебя все получилось!") #результат твоего ответа
                 return 1
-            elif (event.text.lower() == 'подсказка'):
+            elif (event.text.lower() == 'подсказка'): #подсказка в случае неправильного ответа
                 vk.messages.send(
                     user_id=event.user_id,
                     random_id=time.time(),
-                    message="змея!",
-                    attachment = "photo-196126300_457239023")
+                    message="", #сообщение(подсказка)
+                    attachment = "") #ссылка на картинку, видео(подсказка)
             else:
                 vk.messages.send(
                     user_id=event.user_id,
                     random_id=time.time(),
-                    message="Не верный ответ! Можешь запросить подсказку")
+                    message="Не верный ответ! Можешь запросить подсказку") #прозьба о запросе подсказки
 
 def moodFunction():
     for event in longpoll.listen():
@@ -51,7 +51,7 @@ def moodFunction():
                 vk.messages.send(
                     user_id=event.user_id,
                     random_id=time.time(),
-                    message="Не понимаю тебя , повтори пожалуйста")
+                    message="Не понимаю тебя , повтори пожалуйста") #бот не понимает твоих действий 
 
 
 
@@ -59,88 +59,62 @@ def moodFunction():
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         counter(event.user_id)
-        if(event.text.lower()=='!меню'):
+        if(event.text.lower()=='текст'): #значение, которое вы хотите задать
             keys = open("keys.json" , "r" , encoding="UTF-8").read()
             name = users.get(user_ids=event.user_id, fields='city')[0]['first_name']
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="Привет, " + name + " ,я бот, который стараеться любым способом помочь разработчикам игр!!!"
-                                           "\nВот мой список команд:"
-                                           "\n!меню"
-                                           "\n!UE5"
-                                           "\n!UE4"
-                                           "\n!UE5t"
-                                           "\n!Топ 10 ассетов UE4"
-                                           "\n!Топ 10 ассетов Unity"
-                                           "\nДругие команды:"
-                                           "\n!Обработка сообщений"
-                                           "\n!Связь с администрцией"
-                                           "\n!Где я живу?",
-                                            keyboard = keys)
+                message="текст" + name + "текст", keyboard = keys) #можно использовать как приветствие бота при выполнение значения
             moodFunction()
-        elif(event.text.lower() == "!обработка сообщений"):
+        elif(event.text.lower() == "!обработка сообщений"): #сколько бот обработал сообщений
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message=str(getCounter()) + " вот столько. " + str(getUsers()) + " - столько пользователей мне написало")
-        elif (event.text.lower() == "!где я живу?"):
+                message=str(getCounter()) + " вот столько. " + str(getUsers()) + " - столько пользователей мне написало") #получения информации
+        elif (event.text.lower() == "где ты живешь"): #значение, которое может выводить ваш город
             try:
                 city = users.get(user_ids=event.user_id, fields='city')[0]['city']['title']
             except:
-                city = "который не указан"
+                city = "который не указан" #город не высвечивается, если он у вас не указан в профиле вк
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="Ты из города " + city)
+                message="Ты из города " + city) #город высвечивается в процессе выполнения значения
 
-        elif (event.text.lower() == "хочу тест"):
+        elif (event.text.lower() == "тест"): #значение, которое вы хотите задать(в этой строке можно сделать маленький тест). Строчка 85 относится к строчке 30
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="1. На каком языке был сделан Battlefield?")
+                message="текст") #вопрос к тесту выводящийся в процессе выполнения значения
             riddleFunction()#Запуск прослушки
-        elif (event.text.lower() == "!связь с администрацией"):
+        elif (event.text.lower() == "текст"): #значение, которое вы хотите задать
             name = users.get(user_ids=event.user_id, fields='city', name_case="dat")[0]['first_name']
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="Администрация получила оповещение! В скором времени они вам ответят."
-                        " Если хотите что бы они ответили быстрее перейдите по ссылке: \nvk.com/inval1dsyntax")
-        elif (event.text.lower() == "!ue5t"):
+                message="") #вообщение, которое вы получите в процессе выплнения значения(текст)
+        elif (event.text.lower() == "текст"): #значение, которое вы хотите задать
             name = users.get(user_ids=event.user_id, fields='city', name_case="dat")[0]['first_name']
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                attachment = "video-176128467_456239173")
-        elif (event.text.lower() == "!ue4"):
+                attachment = "ссылка") #сообщение, которое вы получите в процессе выполнения значения(фотографи или видео(ссылка))
+        elif (event.text.lower() == "текст"): #значение, которое вы хотите задать
             name = users.get(user_ids=event.user_id, fields='city', name_case="dat")[0]['first_name']
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="UE4 - игровой движок, разрабатываемый и поддерживаемый компанией Epic Games. Один из самых популярных игровых движков."
-                        "\nЯзык программирования: C++"
-                        "\nПоддерживает: Windows 7+ ; MacOS 10.9.2+"
-                        "\nЗанимает места: 22 ГБ (с установками пакетов и ассетов требуеться не меньше 100гб свободного места"
-                        "\nТребования к железу:"
-                        "\n1.64-битная операционная система"
-                        "\n2.Четырехъядерный процессор частотой от 2.5 ГГцNVIDIA GeForce 470 GTX или AMD Radeon 6870 HD и выше"
-                        "\n3.8ГБ оперативной памяти")
-        elif (event.text.lower() == "!unity"):
+                message="текст") #сообщение, которое вы поулчите в процессе выполнения значения(текст)
+        elif (event.text.lower() == "!unity"): #значение, которое ввы хотите задать
             name = users.get(user_ids=event.user_id, fields='city', name_case="dat")[0]['first_name']
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="Unity — межплатформенная среда разработки компьютерных игр, разработанная американской компанией Unity Technologies."
-                        "\nЯзык программирования: C#"
-                        "\nПоддерживает: Windows7+ ; MacOS10.9.2+ ; Linux"
-                        "\nЗанимает места: 14,6 гб"
-                        "\nТребования к железу:"
-                        "\n1.Процессор с поддержкой SSE2"
-                        "\n2.Видеокарта с поддержкой DirectX 9 (модель шейдера 3.0) или DirectX 11 с поддержкой возможностей уровня 9.3.")
+                message="") #сообщение, которе вы получите в процессе выполнения значения(текст)
         else:
             vk.messages.send(
                 user_id=event.user_id,
                 random_id=time.time(),
-                message="Не понял , что ты написал")
+                message="Не понял , что ты написал") #бот не понимает что ты написал
 
